c6aed1f67e8e229f4754c41f670a2ac3daa3b392
ben <jonesr79@gmail.com>
2014-06-30 08:46:47 -0700

    Add basic structure and a couple of crud tiles
    Structure is base on melonjs boilerplate.
    Some cleaning will happen on next commit.

---
+++ app/README.md
@@ -0,0 +1,6 @@
+credits
+audio
+	- http://noproblo.dayjo.org/ZeldaSounds/
+sprites 
+	- http://zs.ffshrine.org/legend-of-zelda/sprites.php
+	- http://www.wiizelda.net/legend-of-zelda/legend-of-zelda-nes-sprites/
---
+++ app/js/resources.js
@@ -0,0 +1,23 @@
+game.resources = [
+
+	/**
+	 * Graphics. 
+	 */
+	//level tileset	 
+	{name: "background_tileset", type: "image", src: "data/map/background_tileset.png"},
+		
+	/**
+	 * Maps. 
+ 	 */
+	{name: "8_G", type: "tmx", src: "data/map/8_G.tmx"}
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
