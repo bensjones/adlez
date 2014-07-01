a4e0bbaac7ccec35089fcd67d40a60f7f1664a2a
ben <jonesr79@gmail.com>
2014-07-01 01:29:35 -0700

    Remove tutorial entity updating
    Need to research how to handle entity movement in the screen.

--- app/js/entities/entities.js
+++ app/js/entities/entities.js
@@ -12,6 +12,7 @@ game.PlayerEntity = me.ObjectEntity.extend({
     init: function(x, y, settings) {
         // call the constructor
         this.parent(x, y, settings);
+
     },
  
     /* -----
@@ -21,33 +22,7 @@ game.PlayerEntity = me.ObjectEntity.extend({
     ------ */
     update: function(dt) {
  
-        if (me.input.isKeyPressed('left')) {
-            // flip the sprite on horizontal axis
-            this.flipX(true);
-            // update the entity velocity
-            this.vel.x -= this.accel.x * me.timer.tick;
-        } else if (me.input.isKeyPressed('right')) {
-            // unflip the sprite
-            this.flipX(false);
-            // update the entity velocity
-            this.vel.x += this.accel.x * me.timer.tick;
-        } else {
-            this.vel.x = 0;
-        }
- 
-        // check & update player movement
-        this.updateMovement();
         
-        // update animation if necessary
-        if (this.vel.x!=0 || this.vel.y!=0) {
-            // update object animation
-            this.parent(dt);
-            return true;
-        }
-         
-        // else inform the engine we did not perform
-        // any update (e.g. position, animation)
-        return false;
     }
  
 });
